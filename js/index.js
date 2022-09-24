17abcce2a34611e83afeaf11c03fa5e90779393a
indra1747 <indrajitpatil1747@gmail.com>
2022-09-25 01:16:25 +0530

    19th review section done

--- js/index.js
+++ js/index.js
@@ -33,6 +33,50 @@ window.onscroll = () => {
 
 }
 
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween:20,
+
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween:20,
+
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
 
 
 
