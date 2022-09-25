a4a14165bef521d8f8a4278e4f2c19e00d92b282
indra1747 <indrajitpatil1747@gmail.com>
2022-09-25 12:49:06 +0530

    16th commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
+let searchForm =document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>
+{
+    searchForm.classList.toggle('active');
+    shopping.classList.remove('active');
+    loginpage.classList.remove('active');
+    navbar.classList.remove('active');
+    
+}
+
+
 let shopping = document.querySelector(".shopping-cart");
 
 document.querySelector('#cart-btn').onclick = () =>
 {
+    searchForm.classList.remove('active');
     shopping.classList.toggle('active');
     loginpage.classList.remove('active');
     navbar.classList.remove('active');
@@ -11,6 +24,7 @@ let loginpage = document.querySelector(".login-form");
 
 document.querySelector('#login-btn').onclick = () =>
 {
+    searchForm.classList.remove('active');
     shopping.classList.remove('active');
     loginpage.classList.toggle('active');
     navbar.classList.remove('active');
@@ -19,13 +33,15 @@ document.querySelector('#login-btn').onclick = () =>
 let navbar = document.querySelector(".navbar");
 
 document.querySelector('#menu-btn').onclick = () =>
-{
+{    
+    searchForm.classList.remove('active');
     shopping.classList.remove('active');
     loginpage.classList.remove('active');
     navbar.classList.toggle('active');
 }
 
 window.onscroll = () => {
+    searchForm.classList.remove('active');
     shopping.classList.remove('active');
     loginpage.classList.remove('active');
     navbar.classList.remove('active');
